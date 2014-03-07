8b1ce2ed2005c2a652fd16482dc567cf96d59ea2
Gilles De Mey <gilles.de.mey@gmail.com>
2014-03-07 00:11:26 -0800

    reverted to local setup

--- glados/connect.js
+++ glados/connect.js
@@ -2,11 +2,12 @@
 
 var sysinfo   = require('../core/utility/system'),
     io        = require('socket.io-client'),
+    settings  = require('../config/settings.json'),
     socket;
 
 exports.connect = function () {
 
-  socket = io.connect('http://glados.michiel.io');
+  socket = io.connect(settings.remote.url + ':' + settings.remote.port || 80);
 
   sysinfo.systemInfo(function (info) {
     console.log('[connect]: uploading system information');
